3d72b60eb8e565b3d92b9d45dcb0302fa3e9c4e6
kevin <liangxinxin001@163.com>
2018-10-11 17:32:23 +0800

    home vc

--- KevinDemo/KevinDemo/Home/HomeVC.swift
+++ KevinDemo/KevinDemo/Home/HomeVC.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class HomeVC: BaseViewController {
+class HomeVC: BaseViewController,UITableViewDelegate,UITableViewDataSource{
+    var tv:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initUI()
 //        initNavigationBar()
 
         // Do any additional setup after loading the view.
@@ -29,5 +31,38 @@ class HomeVC: BaseViewController {
 //        self.navigationItem.titleView = titleView
 //        
 //    }
+    
+    func initUI() {
+        tv = UITableView()
+        tv.frame = view.frame
+        tv.delegate   =  self
+        tv.dataSource = self
+        view.addSubview(tv)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "homecell")
+        cell.textLabel?.text = indexPath.row.description
+        cell.textLabel?.textColor = UIColor.white
+        cell.backgroundColor  = UIColor.randomColor()
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        AlertHelper.showToastWithMessage(message: indexPath.row.description)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    
+    
 
 }
